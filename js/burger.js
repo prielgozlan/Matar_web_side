993b6482945502f538accba5745191e3777b1983
priel gozlan <prielgozlan@gmail.com>
2024-08-04 17:01:13 +0300

    view to scrollToSection1

--- js/burger.js
+++ js/burger.js
@@ -20,3 +20,19 @@ function declareBtns(){
 }
 
 declareBtns();
+
+function scrollToSection1(){
+  document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSection2(){
+  document.getElementById("section2").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSection3(){
+  document.getElementById("section3").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSection4(){
+  document.getElementById("section4").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToSection5(){
+  document.getElementById("section5").scrollIntoView({ behavior: "smooth" });
+}
