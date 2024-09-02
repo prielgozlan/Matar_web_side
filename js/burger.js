eebfb394a31a229def2e2dad729cace7665e7c98
priel gozlan <prielgozlan@gmail.com>
2024-09-02 21:39:40 +0300

    change nav

--- js/burger.js
+++ js/burger.js
@@ -19,27 +19,37 @@ declareBtns();
 function scrollToSection1(){
   document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
   let nav_open = document.querySelector("#nav_open");
-  nav_open.style.display = "none"
+  if(window.innerWidth <= 768){
+    nav_open.style.display = "none"
+    }
 
 
 }
 function scrollToSection2(){
   document.getElementById("section2").scrollIntoView({ behavior: "smooth" });
   let nav_open = document.querySelector("#nav_open");
+  if(window.innerWidth <= 768){
   nav_open.style.display = "none"
+  }
 }
 function scrollToSection3(){
   document.getElementById("section3").scrollIntoView({ behavior: "smooth" });
   let nav_open = document.querySelector("#nav_open");
-  nav_open.style.display = "none"
+  if(window.innerWidth <= 768){
+    nav_open.style.display = "none"
+    }
 }
 function scrollToSection4(){
   document.getElementById("section4").scrollIntoView({ behavior: "smooth" });
   let nav_open = document.querySelector("#nav_open");
-  nav_open.style.display = "none"
+  if(window.innerWidth <= 768){
+    nav_open.style.display = "none"
+    }
 }
 function scrollToSection5(){
   document.getElementById("section5").scrollIntoView({ behavior: "smooth" });
   let nav_open = document.querySelector("#nav_open");
-  nav_open.style.display = "none"
+  if(window.innerWidth <= 768){
+    nav_open.style.display = "none"
+    }
 }
