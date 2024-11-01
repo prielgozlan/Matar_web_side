1706ee2c1a8713c413f42a692cb03adc9a2c6c52
Michael Elisha <mishaelifire@gmail.com>
2024-11-01 14:55:20 +0200

    mobile fixes

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let headerLogoSrc =
   window.innerWidth < 1200 ? "/img/logo-mini.jpg" : "/img/logo.png";
 
 setTimeout(() => {
+  window.scroll(0, 0);
   let headerLogoEl = document.querySelector(".headerLogoImg");
   headerLogoEl.src = window.origin + headerLogoSrc;
 });
